89b30a2d641dc1dabe9bc93af39d18796eda0ecd
pks9862728888 <pks9862728888@gmail.com>
2023-04-07 14:19:50 +0530

    Initial commit

---
+++ src/main/java/com/demo/springbootoauth2democryptoportfolio/SpringBootOauth2DemoCryptoPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootoauth2democryptoportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2DemoCryptoPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2DemoCryptoPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springbootoauth2democryptoportfolio/SpringBootOauth2DemoCryptoPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootoauth2democryptoportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2DemoCryptoPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
